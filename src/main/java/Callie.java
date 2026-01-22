352ef2fa29983ad14997aafc16732a0ee3ec8427
cedarglass <166680116+cedarglass@users.noreply.github.com>
2026-01-22 14:34:01 +0800

    updated chatbot behaviour to repeat commands

--- src/main/java/Callie.java
+++ src/main/java/Callie.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+
 public class Callie {
     public static void main(String[] args) {
-        System.out.println("Hello from Callie!\nWhat can I do for you?\n" +
-                "____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!");
+        Scanner sc = new Scanner(System.in);
+
+        printLine();
+        System.out.println("Hello, I'm Callie! It's great to see you around today.\nWhat can I do for you?\n");
+        printLine();
+
+        // repeat what the user types in (i.e. the input), except for when "bye" is typed
+        while (true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                printLine();
+                System.out.println(" Bye. Hope to see you again soon!");
+                printLine();
+                break;
+            }
+
+            printLine();
+            System.out.println(input);
+            printLine();
+        }
+
+        // shutdown protocols
+        sc.close();
+    }
+
+    private static void printLine() {
+        System.out.println("____________________________________________________________");
     }
 }
