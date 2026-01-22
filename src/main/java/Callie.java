ca5a4d6da742e5ea3599bed3c8de4f2cf342f0cb
cedarglass <166680116+cedarglass@users.noreply.github.com>
2026-01-22 11:21:26 +0800

    instantiated skeletal version of chatbot

---
+++ src/main/java/Callie.java
@@ -0,0 +1,7 @@
+public class Callie {
+    public static void main(String[] args) {
+        System.out.println("Hello from Callie!\nWhat can I do for you?\n" +
+                "____________________________________________________________\n" +
+                "Bye. Hope to see you again soon!");
+    }
+}
